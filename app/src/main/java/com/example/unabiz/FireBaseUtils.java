14bf95e5be9fe9c54e2ff286cef495d463e473af
Joshua-Samjaya <64853644+Joshua-Samjaya@users.noreply.github.com>
2021-04-08 19:37:52 +0800

    firebase

--- app/src/main/java/com/example/unabiz/FireBaseUtils.java
+++ app/src/main/java/com/example/unabiz/FireBaseUtils.java
@@ -62,7 +62,7 @@ public class FireBaseUtils {
     }
 
     public static void retrieveAP_coordinates(final AP_coordinatesCallbackInterface callbackAction) {
-        final HashMap<String,Integer> coordinates_1 = new HashMap<>();
+
         final HashMap<String, HashMap<String,Integer>> coordinates_2 = new HashMap<>();
 
         myDatabaseRef.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -71,15 +71,19 @@ public class FireBaseUtils {
 
 
                 for (DataSnapshot key_snapshot : snapshot.getChildren()) {
+                    //Log.i("children",key_snapshot.toString());
+                    //if (!(key_snapshot.getKey() == "WIFI")) {
+                    //    if (!coordinates_1.isEmpty()){
+                    //        coordinates_1.clear();
+                    //    Log.i("clear","clear");
+                    //    }
+                    //}
 
-                    if (!(key_snapshot.getKey() == "WIFI")) {
-                        if (!coordinates_1.isEmpty()){
-                            coordinates_1.clear();}
-                    }
-
-
+                    HashMap<String,Integer> coordinates_1 = new HashMap<>();
 
                     for (DataSnapshot subkey_xy : key_snapshot.getChildren()){
+
+                        //Log.i("subkey_xy",subkey_xy.toString());
 
                         //Log.i("x_coordinates_barrier1", x_coo.toString());
 
@@ -88,6 +92,9 @@ public class FireBaseUtils {
                             Integer x_coo = Integer.parseInt(subkey_xy.getValue().toString());
                             //Log.i("x_coordinates_barrier2", x_coo.toString());
                             coordinates_1.put(subkey_xy.getKey(),  x_coo);
+                            Log.i("x_coordinates1", coordinates_1.toString());
+                            Log.i("x_check",coordinates_2.toString());
+
 
                         }
 
@@ -96,18 +103,28 @@ public class FireBaseUtils {
                             Integer y_coo = Integer.parseInt(subkey_xy.getValue().toString());
                             //Log.i("y_coordinates_barrier2", y_coo.toString());
                             coordinates_1.put(subkey_xy.getKey(),  y_coo);
+                            Log.i("y_coordinates1", coordinates_1.toString());
+                            Log.i("y_check",coordinates_2.toString());
                         }
 
                         //Log.i("subkey_xy", subkey_xy.getKey());
-                        Log.i("coordinates_1", coordinates_1.toString());
+                        //Log.i("coordinates_1", coordinates_1.toString());
+                        //Log.i("coordinates1", coordinates_1.toString());
+                        //Log.i("final_check",coordinates_2.toString());
 
                     }
-                    coordinates_2.put(key_snapshot.getKey(), coordinates_1);
+                    if(!coordinates_2.containsKey(key_snapshot.getKey())){
+                        Log.i("log","key is already inside");
+                        Log.i("log_key",key_snapshot.getKey());
+                        Log.i("log_xy",coordinates_1.toString());
+                        String key = key_snapshot.getKey();
+                        Log.i("check",coordinates_2.toString());
+                    coordinates_2.put(key, coordinates_1);}
 
-                    //Log.i("coordinates_mappings", coordinates_2.toString() + "\n");
+                    Log.i("coordinates_mappings", coordinates_2.toString() + "\n");
 
                 }
-                Log.i("coordinates_mappings", coordinates_2.toString() + "\n");
+                //Log.i("coordinates_mappings", coordinates_2.toString() + "\n");
 
                 callbackAction.onCallback(coordinates_2);
 
