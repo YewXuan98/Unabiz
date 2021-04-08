5430adc6c920f43cbd6a9607b44cf33d8688cf7b
placid_brain <shwetha_selvam@mymail.sutd.edu.sg>
2021-04-09 00:55:57 +0800

    neural network test file

---
+++ app/src/test/java/com/example/unabiz/NeuralNetworkTest.java
@@ -0,0 +1,20 @@
+package com.example.unabiz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NeuralNetworkTest {
+
+    @Test
+    public void predict() {
+    }
+
+    @Test
+    public void fit() {
+    }
+
+    @Test
+    public void train() {
+    }
+}
